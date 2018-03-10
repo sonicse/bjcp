b1d91f9469cbaf478d38cb06c52ff6ecbc9d9c69
Unknown <sonicse@gmail.com>
2018-03-10 19:28:40 +0500

    v2.0 separate bjcp and groups xml files from strings.xml

--- app/src/main/java/com/sonicse/bjcp/App.java
+++ app/src/main/java/com/sonicse/bjcp/App.java
@@ -2,8 +2,6 @@ package com.sonicse.bjcp;
 
 import android.app.Application;
 
-import com.bettervectordrawable.VectorDrawableCompat;
-import com.sonicse.bjcp.R;
 
 /**
  * Created by sonicse on 20.09.15.
@@ -12,12 +10,5 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        VectorDrawableCompat.enableResourceInterceptionFor(getResources(),
-                R.drawable.magnify,
-                R.drawable.chevron_right,
-                R.drawable.chevron_down,
-                R.drawable.arrow_left,
-                R.drawable.information_outline);
     }
 }
